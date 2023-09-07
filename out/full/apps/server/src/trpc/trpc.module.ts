78bbd2899589ec22c0074aa2e277337ec50907b7
Milos Zarkovic <milos.zarkovic@holywally.com>
2023-09-08 01:12:58 +0200

    delete out

--- out/full/apps/server/src/trpc/trpc.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TrpcService } from '@server/trpc/trpc.service';
-import { TrpcRouter } from '@server/trpc/trpc.router';
-
-@Module({
-  imports: [],
-  providers: [TrpcService, TrpcRouter],
-})
-export class TrpcModule {}
